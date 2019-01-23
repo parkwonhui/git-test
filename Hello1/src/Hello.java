62a63e0bd38c8e24ef9de805c53672310ddcd221
parkwonhui <parkwonhui.dev@gmail.com>
2019-01-23 22:06:09 +0900

    stash에 있던 것 꺼내오기

--- Hello1/src/Hello.java
+++ Hello1/src/Hello.java
@@ -2,9 +2,12 @@
 public class Hello {
 	public static void main(String[] args) {
 		System.out.println("g12312312it");
-		int i = 2+1;
-		int j = 2+1;
-		
+		int i = 1+1;
+		int k = 2;
+		System.out.println("ma2ster");
+		System.out.println("master11");
+		int j = 0;
+
 	}
 
 }

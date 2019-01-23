8a5507b51d177448a48cc30147fecd54af4c61d3
parkwonhui <parkwonhui.dev@gmail.com>
2019-01-23 21:30:39 +0900

    헬로 커밋2

--- Hello1/src/Hello.java
+++ Hello1/src/Hello.java
@@ -3,7 +3,7 @@ public class Hello {
 	public static void main(String[] args) {
 		System.out.println("g12312312it");
 		int i = 1+1;
-		System.out.println("master");
+		System.out.println("ma2ster");
 		System.out.println("master11");
 		System.out.println("master21");
 

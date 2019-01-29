a697cc736b9053bfc6b675a60ed7668eff2f93e1
parkwonhui <parkwonhui.dev@gmail.com>
2019-01-29 21:54:00 +0900

    test3 merge

--- Hello1/src/Hello.java
+++ Hello1/src/Hello.java
@@ -1,11 +1,10 @@
 
 public class Hello {
 	public static void main(String[] args) {
-		System.out.println("g12312312it");
 		int i = 1+1;
 		int k = 2;
-		System.out.println("test3 branch");		
-
+		System.out.println("test3 branch");	
+		System.out.println("test3 merge");	
 	}
 
 }

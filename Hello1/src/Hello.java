3131aa12ba7a204937381b7edefd1c18fe1dc445
parkwonhui <parkwonhui.dev@gmail.com>
2019-01-29 22:01:44 +0900

    충돌 해결

--- Hello1/src/Hello.java
+++ Hello1/src/Hello.java
@@ -1,17 +1,11 @@
 
 public class Hello {
 	public static void main(String[] args) {
-<<<<<<< HEAD
 		System.out.println("master");
 		int i = 2+1;
-		int j = 2+1;
-		
-=======
-		int i = 1+1;
-		int k = 2;
+		int j = 2+1;	
 		System.out.println("test3 branch");	
 		System.out.println("test3 merge");	
->>>>>>> refs/remotes/origin/test3
 	}
 
 }

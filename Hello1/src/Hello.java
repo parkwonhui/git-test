bc7c02735cbeae889eade02c750f7eba6176d98d
parkwonhui <parkwonhui.dev@gmail.com>
2019-01-29 22:15:56 +0900

    test3

--- Hello1/src/Hello.java
+++ Hello1/src/Hello.java
@@ -6,7 +6,8 @@ public class Hello {
 		int j = 2+1;	
 		System.out.println("test3 branch");	
 		System.out.println("test3 merge");	
-		System.out.println("master");	
+		System.out.println("master");
+		System.out.println("test3");	
 		
 	}
 
